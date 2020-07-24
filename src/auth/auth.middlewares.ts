9a0ee59444f5d56c3a9cc5662ed859bef43e99ab
Mark Zaky <markzaky18@gmail.com>
2020-07-24 11:02:49 +0200

    add build script to heroku

--- src/auth/auth.middlewares.ts
+++ src/auth/auth.middlewares.ts
@@ -1,21 +1,17 @@
 import { any } from "@hapi/joi";
-import { Response, NextFunction } from "express";
+import { Response, NextFunction, RequestHandler } from "express";
 import jwt from "jsonwebtoken";
 import { HttpError } from "../app/app.models";
 import { IGetUserAuthInfoRequest } from "./auth.types";
 
-export const checkAuth = (
-	req: IGetUserAuthInfoRequest,
-	res: Response,
-	next: NextFunction
-) => {
+export const checkAuth: RequestHandler = (req, res, next) => {
 	try {
 		const token = req.headers.authorization;
 		if (!token) {
 			throw new Error("Not Authenticated!");
 		}
 		const decodedToken: any = jwt.verify(token, process.env.JWT_SECRET!);
-		req.user = decodedToken.id;
+		(req as IGetUserAuthInfoRequest).user = decodedToken.id;
 		next();
 	} catch (error) {
 		return next(new HttpError("Not Authenticated!", 401));
